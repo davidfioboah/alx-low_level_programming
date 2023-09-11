9cc4915fca548f4705b1c6fd1d021487deda7a86
David Fioboah <davidkemesedi@gmail.com>
2023-09-11 15:10:15 -0700

    letters of alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - print alphabets in lowercase letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lower[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 26;
+
+	while (i >= 0)
+	{
+		putchar(lower[i]);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
