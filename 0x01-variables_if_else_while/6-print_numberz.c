76a3fd8897dd3fa1c20dc62ddba0056b465d39a4
David Fioboah <davidkemesedi@gmail.com>
2023-09-11 14:30:33 -0700

    program that prints all single digit numbers of base 10 starting from 0 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -  single digit numbers
+ *
+ * Description: to print sigle digit numbers in base ten
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num + '0');
+		num++;
+	}
+	putchar("\n");
+	return (0);
+}
