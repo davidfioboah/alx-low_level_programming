dfe25e16f4dc7a53e01a4e19ec21c85cddd4a662
David Fioboah <davidkemesedi@gmail.com>
2023-09-11 14:05:47 -0700

    program that prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  single digit numbers
+ *
+ * Description: to print sigle digit numbers in base ten
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	
+	while (i < 10)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
+	
