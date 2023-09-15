f88075cdf1d44f0776a7734a869db1b38fbc63c3
David Fioboah <davidkemesedi@gmail.com>
2023-09-15 16:54:42 -0700

    fizz-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,19 @@ int main(void)
 	for (n = 1; n < 101; n++)
 	{
 		if (n % 5 == 0 && n % 3 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (n % 5 == 0)
 			if (n == 100)
-				printf("Buzz ");
+				printf("Buzz");
 			else
-				printf("Buzz ");
+				printf("Buzz");
 		else if (n % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", n);
+			printf("%d", n);
 	}
 	printf("\n");
 
-	return 0;
+	return (0);
 }
 
