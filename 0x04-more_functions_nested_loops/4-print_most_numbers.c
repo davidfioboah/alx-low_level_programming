84f5470edc92c7c01bf831ae02534a59365746a1
David Fioboah <davidkemesedi@gmail.com>
2023-09-15 15:01:55 -0700

    integers without 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers  0-9 except 2 and 4 
+ * print_most_numbers - prints numbers 0-9 except 2 and 4
  *
  * return: void
  */
@@ -9,7 +9,7 @@
 void print_most_numbers(void)
 {
 	int c;
-	
+
 	for (c = 0; c <= 9; c++)
 	{
 		if (c != 2 && c != 4)
