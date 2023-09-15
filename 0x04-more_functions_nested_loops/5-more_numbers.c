2f9016cad79eb2fb372367d943df96502daf8a8e
David Fioboah <davidkemesedi@gmail.com>
2023-09-15 15:44:03 -0700

    10 times the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,12 +4,11 @@
  * more_numbers - prints 10 times the numbers
  * return: void
  */
-
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
