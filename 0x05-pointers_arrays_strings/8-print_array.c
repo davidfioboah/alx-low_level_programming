9730d5f094241791bb2bea87e38d572c8435d6d4
David Fioboah <davidkemesedi@gmail.com>
2023-09-18 14:50:58 -0700

    function that prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,6 +4,7 @@
 /**
  * print_array - prints n elements of an array of integers
  * @n: number of elements of the array to be printed
+ * @a: array of integers in question
  */
 
 void print_array(int *a, int n)
